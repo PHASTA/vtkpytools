eb0a9113d1bd85a0eaf4927e02d3e4c685433815
James Wright <james@jameswright.xyz>
2020-08-11 17:08:25 -0600

    hotfix: Add vCutter to package namespace

--- vtkpytools/__init__.py
+++ vtkpytools/__init__.py
@@ -1,5 +1,5 @@
 from .bar2vtk import *
-from .common import getGeometricSeries, unstructuredToPoly, orderPolyDataLine
+from .common import getGeometricSeries, unstructuredToPoly, orderPolyDataLine, vCutter
 
 
 __name__ = "vtkpytools"
