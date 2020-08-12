04d9ed70ba338eb501c8e2a2a6af108fd698605e
James Wright <james@jameswright.xyz>
2020-08-12 14:36:53 -0600

    feat: Add Profile class

--- vtkpytools/__init__.py
+++ vtkpytools/__init__.py
@@ -1,5 +1,6 @@
 from .bar2vtk import *
-from .common import getGeometricSeries, unstructuredToPoly, orderPolyDataLine, vCutter
+from .common import (getGeometricSeries, unstructuredToPoly, orderPolyDataLine,
+                     vCutter, Profile)
 
 
 __name__ = "vtkpytools"
