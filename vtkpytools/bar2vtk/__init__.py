a05f91883f0ecd5e8a470ac7d226a0914291ae24
James Wright <james@jameswright.xyz>
2020-08-19 08:06:18 -0600

    hotfix: Remove calcBoundaryLayerStats from __init__

--- vtkpytools/bar2vtk/__init__.py
+++ vtkpytools/bar2vtk/__init__.py
@@ -1,6 +1,5 @@
 from .core import form2DGrid, computeEdgeNormals
 from .data import (binaryVelbar, binaryStsbar, calcCf, calcReynoldsStresses,
-                   calcBoundaryLayerStats, compute_vorticity, sampleDataBlockProfile,
-                   calcWallShearGradient)
+                   compute_vorticity, sampleDataBlockProfile, calcWallShearGradient)
 
 __name__ = "bar2vtk"
