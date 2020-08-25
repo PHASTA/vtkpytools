05bab0b0d94dec61f39ed1e967bff657b3747c3e
James Wright <james@jameswright.xyz>
2020-08-25 11:23:33 -0600

    feat: Make script installation symlink, remove extension
     - Add scripts_hack function to setup.py
     - Creates symlink of given script into directory _scripts

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import os
 
 version = ''
 with open('vtkpytools/__init__.py') as f:
@@ -6,9 +7,27 @@ with open('vtkpytools/__init__.py') as f:
         if line.startswith('__version__'):
             version = line.split('=')[1].strip().replace("'", '')
 
+# Symlink script names
+SCRIPTS_PATH='./_scripts'
+def scripts_hack(*scripts):
+    ''' Hack around `pip install` temporary directories '''
+    if not os.path.exists(SCRIPTS_PATH):
+        os.makedirs(SCRIPTS_PATH)
+    scripts_path = []
+    for src_path, basename in scripts:
+        dest_path = os.path.join(SCRIPTS_PATH, basename)
+        if not os.path.exists(dest_path):
+            dest_actual_path = os.path.abspath(os.path.join(dest_path, os.pardir))
+            os.symlink(os.path.relpath(src_path, dest_actual_path), dest_path)
+        scripts_path += [dest_path]
+    return scripts_path
+
 setuptools.setup(
     name = "vtkpytools",
     version = version,
     packages = setuptools.find_packages(),
-    scripts = ['./bin/bar2vtk.py', './bin/barsplit.py']
+    scripts = scripts_hack(
+        ('./bin/bar2vtk.py', 'bar2vtk'),
+        ('./bin/barsplit.py', 'barsplit')
+    )
 )
