bcc81e29cec448a0e7fe929baa058dbd32017a42
James Wright <james@jameswright.xyz>
2020-08-24 20:18:16 -0600

    chore: Add version info to package
     - Will read __version__ from __init__.py of vtkpytools package

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 import setuptools
 
+version = ''
+with open('vtkpytools/__init__.py') as f:
+    for line in f.readlines():
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().replace("'", '')
+
 setuptools.setup(
     name = "vtkpytools",
+    version = version,
     packages = setuptools.find_packages()
 )
--- vtkpytools/__init__.py
+++ vtkpytools/__init__.py
@@ -2,5 +2,4 @@ from .bar2vtk import *
 from .common import (getGeometricSeries, unstructuredToPoly, orderPolyDataLine,
                      vCutter, Profile)
 
-
-__name__ = "vtkpytools"
+__version__ = '0.0.1'
