f68b4ffd32c2d74ac63bb9ec692f703f3163876d
James Wright <james@jameswright.xyz>
2020-09-01 10:05:06 -0600

    feat: Add globFile to __init__ from common

--- vtkpytools/__init__.py
+++ vtkpytools/__init__.py
@@ -1,5 +1,5 @@
 from .bar2vtk import *
 from .common import (getGeometricSeries, unstructuredToPoly, orderPolyDataLine,
-                     vCutter, Profile)
+                     vCutter, Profile, globFile)
 
 __version__ = '0.0.2'
