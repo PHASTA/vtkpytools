4a3b57229c2aa92311dbbd0e8bb56efa37c14482
James Wright <james@jameswright.xyz>
2020-11-22 11:10:04 -0700

    chore: Update version number

--- vtkpytools/__init__.py
+++ vtkpytools/__init__.py
@@ -3,4 +3,4 @@ from .common import (getGeometricSeries, unstructuredToPoly, orderPolyDataLine,
                      vCutter, Profile, globFile, rotateTensor, makeRotationTensor,
                      symmetric2FullTensor, full2SymmetricTensor, calcStrainRate)
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
