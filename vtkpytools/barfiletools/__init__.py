6c40701e1e43d246179a3bbe87a9322e9649adb2
James Wright <james@jameswright.xyz>
2021-02-05 19:59:43 -0700

    feat(bar2vtk): Migrate bar2vtk calculation into function
     - The actual computation and argument parsing for bar2vtk has been
       split

--- vtkpytools/barfiletools/__init__.py
+++ vtkpytools/barfiletools/__init__.py
@@ -2,5 +2,6 @@ from .core import form2DGrid, computeEdgeNormals
 from .data import (binaryVelbar, binaryStsbar, calcCf, calcReynoldsStresses,
                    compute_vorticity, sampleDataBlockProfile, calcWallShearGradient,
                    wallAlignRotationTensor)
+from .bar2vtk import bar2vtk
 
 __name__ = "bar2vtk"
