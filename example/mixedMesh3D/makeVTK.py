9bdc90b571c03aba699e78cc8b0a6237bf1c5fa6
James Wright <james@jameswright.xyz>
2021-02-08 13:28:03 -0700

    doc(gridtools3d): Add example of TetWedge topology builder
     - Runs through the same makeVTK.py file, just change the meshtype
       variable

--- example/mixedMesh3D/makeVTK.py
+++ example/mixedMesh3D/makeVTK.py
@@ -13,17 +13,23 @@ import vtkpytools as vpt
 
 #%% ---- File inputs ----
     # Existing files
-coordsPath = Path('./mixedMesh3D.crd')
-connecPath = Path('./mixedMesh3D.cnn')
+# meshtype='HexTetWedge'
+meshtype='TetWedge'
 
-vtkPath = Path('result/mixedmesh3d.vtk')
+coordsPath = Path('./mixedMesh3D_{}.crd'.format(meshtype))
+connecPath = Path('./mixedMesh3D_{}.cnn'.format(meshtype))
+
+vtkPath = Path('result/mixedmesh3d_{}.vtk'.format(meshtype))
 
 #%% ---- Building the VTK Grid object ----
 coords = np.loadtxt(coordsPath, skiprows=1)
 coords = coords[:,:]
 
 connec = np.loadtxt(connecPath, dtype=np.int64, skiprows=1)
-connec = connec[:,:] -1
+connec = connec[:,:] -1 # node index must be index-by-0, so subtracting one from my index-by-1 array
+
+# The coords and connec arrays should be [nen, 3] and [nen, nnodes per element] in shape
+# Feel free to check via 'coords.shape' or 'connect.shape'.
 
     # Create the 2D grid from the given information
 grid = vpt.form3DGrid(coords, connectivity_array=connec)
