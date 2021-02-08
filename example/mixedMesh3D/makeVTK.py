23959beedb04124caa890ad99bdb67a1ffed8be9
James Wright <james@jameswright.xyz>
2021-02-08 13:37:37 -0700

    doc(gridtools3d): Add comment explaining meshtype variable
     - Add comment inside makeVTK.py example script explaining the purpose
       of the meshtype variable

--- example/mixedMesh3D/makeVTK.py
+++ example/mixedMesh3D/makeVTK.py
@@ -12,8 +12,7 @@ sys.path.insert(0, '../../')
 import vtkpytools as vpt
 
 #%% ---- File inputs ----
-    # Existing files
-# meshtype='HexTetWedge'
+# Change meshtype to either "HexTetWedge" or 'TetWedge' to run either example
 meshtype='TetWedge'
 
 coordsPath = Path('./mixedMesh3D_{}.crd'.format(meshtype))
@@ -21,16 +20,17 @@ connecPath = Path('./mixedMesh3D_{}.cnn'.format(meshtype))
 
 vtkPath = Path('result/mixedmesh3d_{}.vtk'.format(meshtype))
 
-#%% ---- Building the VTK Grid object ----
+#%% ---- Read in Files ----
 coords = np.loadtxt(coordsPath, skiprows=1)
 coords = coords[:,:]
 
 connec = np.loadtxt(connecPath, dtype=np.int64, skiprows=1)
-connec = connec[:,:] -1 # node index must be index-by-0, so subtracting one from my index-by-1 array
+connec = connec[:,:] - 1 # node index must be index-by-0, so subtracting one from my index-by-1 array
 
 # The coords and connec arrays should be [nen, 3] and [nen, nnodes per element] in shape
 # Feel free to check via 'coords.shape' or 'connect.shape'.
 
+#%% ---- Building the VTK Grid object ----
     # Create the 2D grid from the given information
 grid = vpt.form3DGrid(coords, connectivity_array=connec)
 grid.save(vtkPath)
