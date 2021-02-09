9cb3af912da8b44978e3df83a3d1e978281a6d1c
James Wright <james@jameswright.xyz>
2021-02-09 15:31:00 -0700

    test: Create first test

---
+++ tests/test_common.py
@@ -0,0 +1,15 @@
+import pytest
+import numpy as np
+
+import vtkpytools as vpt
+
+def test_calcStrainRate():
+    gradientarray = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9],
+                              [5, 8, 7, 3, 2, 8, 9, 5, 1]])
+    solution = np.array([[1, 5, 9,   3, 5,   7],
+                         [5, 2, 1, 5.5, 8, 6.5]])
+    strainrate = vpt.calcStrainRate(gradientarray)
+
+    assert np.allclose(strainrate, solution)
+
+
