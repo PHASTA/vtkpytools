8d1b3288fec1a619506e33943c93360afc5ef844
James Wright <james@jameswright.xyz>
2021-02-09 20:37:43 -0700

    test(bar2vtk): Implement test of example directory

--- tests/barfiletools/test_bar2vtk.py
+++ tests/barfiletools/test_bar2vtk.py
@@ -1,8 +1,10 @@
 import pytest
 from unittest import TestCase
+import shutil, os, sys
 
 import vtkpytools as vpt
 from pathlib import Path, PurePath
+
 
 def test_convertArray2TomlTypes():
     test = {'vtkfile': Path('result/exampleMesh.vtm'),
@@ -21,3 +23,16 @@ def test_convertArray2TomlTypes():
     vpt.barfiletools.bar2vtk._convertArray2TomlTypes(test, convertArray)
 
     TestCase().assertDictEqual(test, solution)
+
+@pytest.fixture()
+def fixture_bar2vtk_exampledirectory(tmp_path):
+    shutil.copytree('./example/bar2vtk/data', tmp_path / 'data')
+    shutil.copytree('./example/bar2vtk/meshFiles', tmp_path / 'meshFiles')
+    shutil.copy('./example/bar2vtk/makeVTM.py', tmp_path)
+    os.chdir(tmp_path)
+    exec(open('makeVTM.py').read())
+
+def test_bar2vtk_exampledirectory(fixture_bar2vtk_exampledirectory, tmp_path):
+    sys.argv = 'bar2vtk cli result/exampleMesh.vtm data 10000'.split(' ')
+    vpt.bar2vtk_bin()
+    pass
