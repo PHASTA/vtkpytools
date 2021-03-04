65e014c93f5b75bfb0f788637e7787334f0004e9
James Wright <james@jameswright.xyz>
2021-03-04 09:30:43 -0700

    feat: Add regex capability to globFile
     - Also added appropriate tests to check it

--- tests/test_common.py
+++ tests/test_common.py
@@ -2,6 +2,7 @@ import pytest
 import numpy as np
 
 import vtkpytools as vpt
+from pathlib import Path
 
 
 @pytest.fixture
@@ -40,3 +41,23 @@ def test_full2SymmetricTensor(symAndFullTensor):
 
     sym_test = vpt.full2SymmetricTensor(full)
     assert np.allclose(sym_test, sym)
+
+@pytest.fixture(params=['', '.1'])
+def createGlobFiles(request, tmp_path):
+    testStrings = ['velbar.20000',
+                   'velbar.20000-30000',
+                   'velbar.200',
+                   'velbar.200-30000']
+    for testString in testStrings:
+        testPath = tmp_path / Path(testString + request.param)
+        testPath.touch()
+
+# Should fail
+def test_globFile_glob(createGlobFiles, tmp_path):
+    with pytest.raises(RuntimeError):
+        vpt.globFile(r'velbar*.200*', tmp_path)
+
+def test_globFile_regex(createGlobFiles, tmp_path):
+    print(list(tmp_path.iterdir()))
+    vpt.globFile(r'^velbar\.200(?![\d|-]).*$', tmp_path, regex=True)
+
