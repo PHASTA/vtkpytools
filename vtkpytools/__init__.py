6a2934a360c15c76e8dcbf77a2bd8951821713a5
James Wright <james@jameswright.xyz>
2021-03-04 08:28:29 -0700

    Further WIP

--- vtkpytools/__init__.py
+++ vtkpytools/__init__.py
@@ -4,5 +4,5 @@ from .gridtools2d import *
 from .common import (getGeometricSeries, unstructuredToPoly, orderPolyDataLine,
                      vCutter, Profile, globFile, rotateTensor, makeRotationTensor,
                      symmetric2FullTensor, full2SymmetricTensor, calcStrainRate)
-from .bl import (integratedVortBLThickness)
+from .bl import (integratedVortBLThickness, sampleBLStuff, delta_momentum)
 
--- vtkpytools/bl.py
+++ vtkpytools/bl.py
@@ -3,6 +3,11 @@ import pyvista as pv
 import warnings
 from .barfiletools import *
 from scipy.integrate import cumtrapz
+from numpy import ndarray
+
+# For debugging only
+import IPython.core.debugger as ipdb
+# import ipdb
 
 
 def calcBoundaryLayerStats(dataBlock, line_walldists, bl_functions, velocity_name, normals):
@@ -22,6 +27,21 @@ def calcBoundaryLayerStats(dataBlock, line_walldists, bl_functions, velocity_nam
     # dataBlock['wall']['Re_theta'] = Re_theta
 
     return dataBlock
+
+def sampleBLStuff(dataBlock, sample_dists, vectors, locations) -> pv.PolyData:
+
+    nlocations = locations.shape[0]
+    ntotsamples = nlocations*len(sample_dists)
+    nprofilesamples = len(sample_dists)
+    profiles = np.einsum('i,jk->jik', sample_dists, vectors)
+    profiles += locations[:, None, :]
+    profile_pnts = profiles.reshape(ntotsamples, 3)
+
+    sample_pnts = pv.wrap(profile_pnts)
+    sample_pnts = sample_pnts.sample(dataBlock['grid'])
+    sample_pnts['WallDistance'] = np.repeat(sample_dists, locations.shape[0])
+    # Tracer()()
+    return sample_pnts
 
 def integratedVortBLThickness(vorticity, wall_distance, delta_percent=0.995,
                          delta_displace=False, delta_momentum=False) -> dict:
@@ -44,6 +64,22 @@ def integratedVortBLThickness(vorticity, wall_distance, delta_percent=0.995,
         delta_momentum -= delta_displace_lambda()
 
     return {'delta_percent':delta_percent, 'delta_displace':delta_displace, 'delta_momentum':delta_momentum}
+
+# def cumulativeIntegratedVorticity(sample_pnts)
+#
+
+def delta_momentum(U: ndarray, wall_distance: ndarray, nwallpnts: int) -> ndarray:
+    samples_per_wallpnt = int(U.size/nwallpnts)
+
+    U = U.reshape(nwallpnts, samples_per_wallpnt)
+    wall_distance = wall_distance.reshape(nwallpnts, samples_per_wallpnt)
+
+    Uedge = U[:, -1]
+    # Uedge = np.ones((U.shape[0]))
+
+    delta_mom = np.trapz((1 - U/Uedge[:,None])*(U/Uedge[:,None]), wall_distance, axis=1)
+    # ipdb.set_trace()
+    return delta_mom
 
 
 # def velocityBLThickness(U, wall_distance, delta_percent=0.995,
