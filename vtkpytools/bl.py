60f3acff910490d20a2be6fbecebc5265ea6529d
James Wright <james@jameswright.xyz>
2021-03-25 20:20:01 -0600

    feat(bl): Implement delta_vortInt function

--- vtkpytools/bl.py
+++ vtkpytools/bl.py
@@ -51,27 +51,95 @@ def sampleAlongVectors(dataBlock, sample_dists, vectors, locations) -> pv.PolyDa
     sample_pnts['WallDistance'] = np.tile(sample_dists, locations.shape[0])
     return sample_pnts
 
-def integratedVortBLThickness(vorticity, wall_distance, delta_percent=0.995,
-                         delta_displace=False, delta_momentum=False) -> dict:
-        # Use eqns 3.1-3.4 in Numerical study of turbulent separation bubbles with varying pressure gradient and Reynolds number
-    U = -cumtrapz(vorticity, wall_distance, initial=0)
-    Ue = U[-1]
 
-    percent_index = np.argmax(U > delta_percent*Ue)
-    delta_percent = np.interp(delta_percent*Ue, U[percent_index-1:percent_index+1],
-                                    wall_distance[percent_index-1:percent_index+1])
+def delta_vortInt(vorticity, wall_distance, nwallpnts: int, displace=False,
+                  momentum=False, returnUvort=False, Uedge=None) -> dict:
+    """Calculate vorticity-integrated BL thicknesses (momentum and displacement)
 
-    delta_displace_lambda = lambda: -(1/Ue) * np.trapz(wall_distance*vorticity, wall_distance)
-    if delta_displace:
+    Based on eqns 3.1-3.4 in "Numerical study of turbulent separation bubbles
+    with varying pressure gradient and Reynolds number" Coleman et. al. 2018
+
+    First, we define U as the cumulative integral of vorticity from the wall:
+
+        U_i(y) = \int_0^y vorticity_i(j) dj
+
+    where i indexes the wall point and y is distance from the wall. This is
+    Uvort (which is returned by setting 'returnUvort' to True) and can be used
+    to find delta_percent.
+
+        result = vpt.delta_vortInt(...., returnUvort=True)
+        delta_percent_vorticity = delta_percent(U=result['Uvort'], ....)
+
+    Displacement thickness, d_i, is defined as:
+
+       d_i = -1/Uedge_i \int y * vorticity_i(y) dy
+
+    over the full profile height, with i indexing the wall point.
+
+    Momentum thickness, m_i, is defined as:
+
+       m_i = -2/Uedge_i^2 \int [y * U_i(y) * vorticity_i(y)]dy - d_i
+
+    over the full profile height, with i indexing the wall point. Note that U_i
+    is the vorticity-defined velocity defined above.
+
+    Parameters
+    ----------
+    vorticity : [nwallpnts*nprofilesamples] ndarray
+        Spanwise component of vorticity of the flow.
+    wall_distance : [nwallpnts*nprofilesamples] ndarray
+        Distance to wall for all the sample points
+    nwallpnts : int
+        Number of wall locations used in the sampling of U. The size of U and
+        wall_distance must be evenly divisible by nwallpnts.
+    displace : bool, optional
+        Whether to calculate displacement thickness
+    momentum : bool, optional
+        Whether to calcualte momentum thickness
+    Uedge : [nwallpnts] ndarray, optional
+        Sets the values for the edge (vorticity-integrated) velocity used in calculating the boundary
+        layer height. If not given, the last sample point for each wall point
+        profile will be used (Default: None).
+
+    Returns
+    -------
+    Dictionary with the following items optionally defined:
+
+    delta_displace: ndarray, optional
+        Displacement thickness. Not passed if displace=False
+
+    delta_momentum: ndarray, optional
+        Momentum thickness. Not passed if momentum=False
+
+    Uvort: ndarray, optional
+        Vorticity-integrated velocity. Not passed if returnUvort=False
+
+    """
+
+    if vorticity.size % nwallpnts != 0:
+        raise ValueError('Number of data points ({}) not evenly divisible by '
+                         'nwallpnts ({}). Cannot reshape array.'.format(vorticity.size, nwallpnts))
+
+    vorticity = vorticity.reshape(nwallpnts, -1)
+    wall_distance = wall_distance.reshape(nwallpnts, -1)
+
+    U = -cumtrapz(vorticity, wall_distance, initial=0, axis=1)
+    Uedge = U[:, -1] if Uedge is None else Uedge
+
+    delta_displace_lambda = lambda: -(1/Uedge) * np.trapz(wall_distance*vorticity, wall_distance, axis=1)
+
+    results = {}
+    if displace:
         delta_displace = delta_displace_lambda()
-        if delta_momentum:
-            delta_momentum = -(2/Ue**2) * np.trapz(U*wall_distance*vorticity, wall_distance)
-            delta_momentum -= delta_displace
-    elif delta_momentum:
-        delta_momentum = -(2/Ue**2) * np.trapz(U*wall_distance*vorticity, wall_distance)
-        delta_momentum -= delta_displace_lambda()
+        results['delta_displace'] = delta_displace
+    if momentum:
+        delta_momentum = -(2/Uedge**2) * np.trapz(U*wall_distance*vorticity, wall_distance, axis=1)
+        delta_momentum -= delta_displace if displace else delta_displace_lambda()
 
-    return {'delta_percent':delta_percent, 'delta_displace':delta_displace, 'delta_momentum':delta_momentum}
+        results['delta_momentum'] = delta_momentum
+    if returnUvort:
+        results['Uvort'] = U
+    return results
 
 
 def delta_velInt(U, wall_distance, nwallpnts: int,
@@ -188,3 +256,26 @@ def delta_percent(U, wall_distance, nwallpnts: int, percent: float, Uedge=None) 
     slopes = (wall_distance[W, index] - wall_distance[W, index -1]) / (U[W, index] - U[W, index -1])
 
     return wall_distance[W, index-1] + (percent*Uedge - U[W, index-1])*slopes
+
+
+def integratedVortBLThickness(vorticity, wall_distance, delta_percent=0.995,
+                         delta_displace=False, delta_momentum=False) -> dict:
+        # Use eqns 3.1-3.4 in Numerical study of turbulent separation bubbles with varying pressure gradient and Reynolds number
+    U = -cumtrapz(vorticity, wall_distance, initial=0)
+    Ue = U[-1]
+
+    percent_index = np.argmax(U > delta_percent*Ue)
+    delta_percent = np.interp(delta_percent*Ue, U[percent_index-1:percent_index+1],
+                                    wall_distance[percent_index-1:percent_index+1])
+
+    delta_displace_lambda = lambda: -(1/Ue) * np.trapz(wall_distance*vorticity, wall_distance)
+    if delta_displace:
+        delta_displace = delta_displace_lambda()
+        if delta_momentum:
+            delta_momentum = -(2/Ue**2) * np.trapz(U*wall_distance*vorticity, wall_distance)
+            delta_momentum -= delta_displace
+    elif delta_momentum:
+        delta_momentum = -(2/Ue**2) * np.trapz(U*wall_distance*vorticity, wall_distance)
+        delta_momentum -= delta_displace_lambda()
+
+    return {'delta_percent':delta_percent, 'delta_displace':delta_displace, 'delta_momentum':delta_momentum}
